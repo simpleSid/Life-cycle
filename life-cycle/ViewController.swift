e96e30c790b215d3e23148cb2f6e4d675c7bba3f
Денис Сидоренко <itklass@mail.ru>
2019-05-03 15:00:05 +0300

    app life-cycle

---
+++ life-cycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  life-cycle
+//
+//  Created by Денис Сидоренко on 03/05/2019.
+//  Copyright © 2019 Денис Сидоренко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
